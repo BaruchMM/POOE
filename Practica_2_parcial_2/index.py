3dd183e66d61a41d184028e7c7130e3e75f3d092
BaruchMM <91707238+BaruchMM@users.noreply.github.com>
2022-04-02 23:19:13 -0600

    s

--- Practica_2_parcial_2/index.py
+++ Practica_2_parcial_2/index.py
@@ -1,7 +1,7 @@
 # %% 
 import read_txt as read
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 # %% 
 path = '/home/baruch/Documentos/GitHub/POOE/Practica_2_parcial_2/'
 fileName = 'text.txt'
@@ -9,4 +9,16 @@ fileName = 'text.txt'
 # %%
 text = open(path + fileName, encoding="utf8") 
 print(text)
+# %%
+hist = {'the':0, 'and':0, 'it':0, 'in':0, 'on':0}
+# %%
+for i in text:
+    for letra in i:
+        letra = letra.lower()
+        if letra in hist.keys():
+            hist[letra] += 1 
+
+# %%
+plt.bar(x=hist.keys(), height=hist.values(), width=1, edgecolor="black")
+
 # %%
